b953a5e8a35bdee59fae4d9a84daad007cf1139e
José Maria Labarta <59398281+jmlcas@users.noreply.github.com>
2026-08-31 15:19:46 +0200

    Remove Collabora Online settings from seahub_settings
    Removed Collabora Online configuration settings.

--- seahub_settings.py
+++ seahub_settings.py
@@ -1,6 +1,1 @@
-# Collabora Online
-ENABLE_COLLABORA = True
-COLLABORA_SERVER = 'https://collabora.dominio.com'
-COLLABORA_SERVER_PORT = 9980
-COLLABORA_SERVER_HTTPS = True
-COLLABORA_FILE_SERVER_ROOT = 'https://app.midominio.com'
+
