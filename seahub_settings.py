e9f0e22a0a9d0f4856f3186066f7497514606865
José Maria Labarta <59398281+jmlcas@users.noreply.github.com>
2026-08-31 10:24:52 +0200

    Add Collabora Online configuration settings

---
+++ seahub_settings.py
@@ -0,0 +1,6 @@
+# Collabora Online
+ENABLE_COLLABORA = True
+COLLABORA_SERVER = 'https://collabora.dominio.com'
+COLLABORA_SERVER_PORT = 9980
+COLLABORA_SERVER_HTTPS = True
+COLLABORA_FILE_SERVER_ROOT = 'https://app.midominio.com'
